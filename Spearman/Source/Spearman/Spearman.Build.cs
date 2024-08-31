1c3206081b68e0e95c241aefa5cebfd527dc0393
Alice <busan0617@gmail.com>
2024-08-31 20:07:11 +0900

    Revert "Merge branch 'main' of https://github.com/Kungya/S1"
    This reverts commit bd384b34da64894d6915965d4003df950109780e, reversing
    changes made to 38e5035050a1d3e13831eb2ce7898b8c13abcd96.

--- Spearman/Source/Spearman/Spearman.Build.cs
+++ Spearman/Source/Spearman/Spearman.Build.cs
@@ -10,7 +10,7 @@ public class Spearman : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "NavigationSystem", "AIModule", "GameplayTasks", "GameplayTags", "Paper2D", "OnlineSubsystemSteam", "OnlineSubsystem", "MultiplayerSubsystem", "NetCore" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] { "ReplicationGraph" });
+        PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
