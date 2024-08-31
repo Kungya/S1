fd5ebe407bc67cbc0ff56485cc521d6db48fecb0
Alice <busan0617@gmail.com>
2024-08-31 20:18:15 +0900

    Init ReplicationGraph Plugin
    Init ReplicationGraph Plugin

--- Spearman/Source/Spearman/Spearman.Build.cs
+++ Spearman/Source/Spearman/Spearman.Build.cs
@@ -10,7 +10,7 @@ public class Spearman : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "UMG", "NavigationSystem", "AIModule", "GameplayTasks", "GameplayTags", "Paper2D", "OnlineSubsystemSteam", "OnlineSubsystem", "MultiplayerSubsystem", "NetCore" });
 
-        PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PrivateDependencyModuleNames.AddRange(new string[] { "ReplicationGraph" });
 
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
